16ea19a2a64007536ba0e09217ba407fc88d1c93
DarnokGitHub <konradp.web@gmail.com>
2024-12-29 15:23:51 +0100

    Adding rendering and the rest

---
+++ MotivationalSlider.js
@@ -0,0 +1,25 @@
+import { motivations } from "./data/Motivations.js";
+
+const imgEl = document.querySelector('img');
+const textEl = document.querySelector('.js-text');
+const authorEl = document.querySelector('.text-author');
+
+let count = 0;
+
+renderPage();
+
+function renderPage(){
+  const {quote, author, image} = motivations[count];
+  imgEl.src = image;
+  textEl.innerHTML = quote;
+  authorEl.innerHTML = author;
+  count++;
+  if (count === motivations.length){
+    count = 0;
+  }
+  
+  setTimeout(()=>{
+    renderPage();
+  },10000);
+}
+
--- data/Motivations.js
+++ data/Motivations.js
@@ -1,16 +1,14 @@
-motivations =[{
+export const motivations =[{
   quote: 'You must be the change you wish to see in the world.',
   author: 'Mahatma Gandhi',
-  image: '../images/Franklin.jpg',
-  id: '1'
+  image: 'images/Gandhi.jpg'
 }, {
   quote: 'Spread love everywhere you go. Let no one ever come to you without leaving happier.',
   author: 'Mother Teresa',
-  image: '../images/Teresa.jpg',
-  id: '2'
+  image: 'images/Teresa.jpg'
 },{
   quote: 'The only thing we have to fear is fear itself.',
   author: 'Franklin D. Roosevelt',
-  image: '../images/Franklin.jpg',
-  id: '3'
+  image: 'images/Franklin.jpg'
 }];
+
