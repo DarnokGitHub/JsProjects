534cb865a81ccfbe9f6c0213f23e4ad2e829f077
DarnokGitHub <konradp.web@gmail.com>
2024-12-29 14:36:54 +0100

    Css_and_Html

---
+++ data/Motivations.js
@@ -0,0 +1,16 @@
+motivations =[{
+  quote: 'You must be the change you wish to see in the world.',
+  author: 'Mahatma Gandhi',
+  image: '../images/Franklin.jpg',
+  id: '1'
+}, {
+  quote: 'Spread love everywhere you go. Let no one ever come to you without leaving happier.',
+  author: 'Mother Teresa',
+  image: '../images/Teresa.jpg',
+  id: '2'
+},{
+  quote: 'The only thing we have to fear is fear itself.',
+  author: 'Franklin D. Roosevelt',
+  image: '../images/Franklin.jpg',
+  id: '3'
+}];
